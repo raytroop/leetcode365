65526ad88043e2c589bf0c40faeb8909a0152386
raytroop <raytroop@gmail.com>
2019-11-19 11:37:41 +0800

    combine.cpp

--- combine.cpp
+++ combine.cpp
@@ -8,20 +8,22 @@ public:
     vector<vector<int>> combine(int n, int k) {
         n_ = n;
         k_ = k;
-        vector<vector<int>> ans;
         vector<int> curr;
+        vector<vector<int>> ans;
         dfs(curr, ans, 1);
-
         return ans;
     }
+
 private:
     int n_;
     int k_;
-    void dfs(vector<int>& curr, vector<vector<int>>& ans, int s){
-        if(curr.size() == k_)
+    void dfs(vector<int>& curr, vector<vector<int>>& ans, int s) {
+        if (curr.size() == k_) {
             ans.push_back(curr);
+            return;
+        }
 
-        for(int i = s; i <= n_; ++i){
+        for (int i = s; i < n_ + 1; ++i) {
             curr.push_back(i);
             dfs(curr, ans, i + 1);
             curr.pop_back();
