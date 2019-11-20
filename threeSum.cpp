48b2425de3cc0e97e4cff98568a5662f66c3dcb5
raytroop <raytroop@gmail.com>
2019-11-20 09:43:09 +0800

    3sum

--- threeSum.cpp
+++ threeSum.cpp
@@ -6,33 +6,26 @@ using std::vector;
 
 // https://zxi.mytechroad.com/blog/two-pointers/leetcode-15-3sum/
 vector<vector<int>> threeSum(vector<int>& nums) {
-    const int n = nums.size();
     std::sort(nums.begin(), nums.end());
+    const int n = nums.size();
     vector<vector<int>> ans;
-    for(int i = 0; i <= n-3; ++i){
-        if (nums[i] > 0)
-            break;
-        if(i > 0 && nums[i] == nums[i-1])
-            continue;
 
-        int target = -nums[i];
+    for (int i = 0; i < n - 2; ++i) {
+        if (nums[i] > 0) break;
+        if (i > 0 && nums[i] == nums[i - 1]) continue;
+
         int l = i + 1;
         int r = n - 1;
-        while(l < r){
-            int sum = nums[l] + nums[r];
-            if(sum < target)
-                ++l;
-            else if(sum > target)
-                --r;
-            else{
-                ans.push_back({nums[i], nums[l], nums[r]});
-                while(l < r && nums[l+1] == nums[l])
-                    ++l;
-                while(l < r && nums[r-1] == nums[r])
-                    --r;
-                ++l;
-                --r;
-            }
+        while (l < r) {
+            int sum = nums[i] + nums[l] + nums[r];
+            if (sum == 0) {
+                ans.push_back({nums[i], nums[l++], nums[r--]});
+                while (l < r && nums[l] == nums[l - 1]) l++;
+                while (l < r && nums[r] == nums[r + 1]) r--;
+            } else if (sum > 0)
+                r--;
+            else
+                l++;
         }
     }
 
